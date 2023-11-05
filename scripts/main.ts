0988b9427ac5275a245b502847fdd16d0466ac30
Mason Flint <masonflint44@gmail.com>
2023-11-05 00:49:33 -0500

    set up resource pack for custom villager helmets

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,36 +1,43 @@
-import { world, EntityQueryOptions } from "@minecraft/server";
+import { EntityQueryOptions, world, system } from "@minecraft/server";
 
-
+// TODO: add check to not re-add villagers to the same army
+// TODO: handle if villager is a soldier in another army
 world.beforeEvents.chatSend.subscribe((eventData) => {
-	const player = eventData.sender;
-	const message = eventData.message;
+  const player = eventData.sender;
+  const message = eventData.message;
 
-	if (message.startsWith('!addToArmy')) {
-		eventData.cancel = true; // Cancel the chat message to not show it in chat
+  if (message.startsWith("!addToArmy")) {
+    eventData.cancel = true; // Cancel the chat message to not show it in chat
 
-		// Extract the army name from the command
-		const armyName = message.split(' ')[1];
-		if (!armyName) {
-			player.runCommandAsync('tellraw @s {"rawtext":[{"text":"Please specify an army name."}]}');
-			return;
-		}
+    // Extract the army name from the command
+    const armyName = message.split(" ")[1];
+    if (!armyName) {
+      player.runCommandAsync('tellraw @s {"rawtext":[{"text":"Please specify an army name."}]}');
+      return;
+    }
 
-        const searchDistance = 8;
-        const villagers = world.getDimension("overworld").getEntities({
-                closest: 1,
-                location: player.location,
-                maxDistance: searchDistance,
-                type: "villager_v2"
-            } as EntityQueryOptions
-        )
-        if (villagers.length == 0) {
-            player.runCommandAsync(`tellraw @s {"rawtext":[{"text":"No nearby villagers to recruit."}]}`);
-            return;
-        }
-        const villager = villagers[0]
+    const searchDistance = 8;
+    const villagers = world.getDimension("overworld").getEntities({
+      closest: 1,
+      location: player.location,
+      maxDistance: searchDistance,
+      type: "villager_v2",
+    } as EntityQueryOptions);
+    if (villagers.length == 0) {
+      player.runCommandAsync(`tellraw @s {"rawtext":[{"text":"No nearby villagers to recruit."}]}`);
+      return;
+    }
+    const villager = villagers[0];
 
-        // Add villager to the specified army
-        villager.setDynamicProperty("armyName", armyName);
-		player.runCommandAsync(`tellraw @a {"rawtext":[{"text":"A new soldier has been recruited for the ${armyName} army!"}]}`);
-	}
+    // Add villager to the specified army
+    villager.setDynamicProperty("armyName", armyName);
+    // Use system.run(...) to trigger event (reference: https://wiki.bedrock.dev/scripting/script-server.html#beforeevents-privilege-system)
+    system.run(() => {
+      villager.triggerEvent("minecraft:become_soldier");
+    });
+
+    player.runCommandAsync(
+      `tellraw @a {"rawtext":[{"text":"A new soldier has been recruited for the ${armyName} army!"}]}`
+    );
+  }
 });
